242012cfe7a5a21d5cc433967dc92ef7439523fe
Jahir Alam <jahiralam@Jahirs-MacBook-Pro.local>
2019-05-10 00:43:47 +0600

    nn

---
+++ precache-manifest.d0a7cd780d3c8b31299f1783ca790179.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "454976ab29eca524c017",
+    "url": "/static/css/main.b58cea90.chunk.css"
+  },
+  {
+    "revision": "454976ab29eca524c017",
+    "url": "/static/js/main.49fd6e23.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9871f4ca0dab55ebc5d1",
+    "url": "/static/js/2.481ca6fc.chunk.js"
+  },
+  {
+    "revision": "5dc040ef54cad7072c9848e491056001",
+    "url": "/index.html"
+  }
+];
